27100982875827892d4ac3e40ec9a820930a8b66
David Fletcher <davidfletcher@Davids-Air-2.lan>
2021-10-08 10:40:23 -0700

    liking the fit of the info, still very tight

--- src/components/Character.js
+++ src/components/Character.js
@@ -5,20 +5,35 @@ import react from "react"; //TODO: doubt i'll need to add a useState or effect h
 import styled, {keyframes} from "styled-components";
 
 const StyledDiv = styled.div`
+    display: flex;
     border: 1px solid black;
+    flex-flow: column wrap;
+    justify-content: center;
+    margin: 2%;
+    height: 10%;
+    width: fit-content;
 
+    //TODO: add a transition
     
-    span {
+    div {
         color: ${pr => pr.theme.textColor};
+        display: flex;
         
         h1{
             font: 30pt News Gothic, sans-script;
+            margin: 0;
             
         }
         button{
+            align-self: center;
+            height: fit-content;
             background-color: ${pr => pr.theme.backColor};
             color: ${pr => pr.theme.textColor};
-
+            
+            transition: 0.2s ease-in-out;
+            &:hover{
+                
+            }
         }
     }
 `
@@ -30,10 +45,10 @@ export default function Character(props){
 
     return(
         <StyledDiv>
-            <span>
+            <div>
                 <h1>{name}</h1>
                 <button >{birthyear}</button>  {/* *****TODO onClick={(() => onClick)} **** */}
-            </span>
+            </div>
         </StyledDiv>
     );
 
