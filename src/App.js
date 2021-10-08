bc45e5d47d447441f44ddcc7b9928f2a34e1c2eb
David Fletcher <davidfletcher@Davids-Air-2.lan>
2021-10-08 09:18:58 -0700

    getting all data i need from my test map function -- set up my map for the character component

--- src/App.js
+++ src/App.js
@@ -10,29 +10,41 @@ import axios from 'axios';
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-   const [ charData, setCharData ] = useState() //!will i need an array? probably..
+   const [ charData, setCharData ] = useState([]) //!will i need an array? probably..
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  //TODO: useEffect(() => {
+  useEffect(() => {
     function getData(){
       axios.get(`https://swapi.dev/api/people`)
       .then( res => {
-        console.log(res)
+        //console.log(res.data)  //! datums within array of data within objects
+        setCharData(res.data)
       })
       .catch(err => {
         console.error(err)
       })
     }
     getData()
-  //TODO: }, [])
+   }, [])
 
+  console.log(charData)
+
+  charData.map(el => {
+    console.log(el.name)
+    console.log(el.birth_year)
+  })
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {/* {charData.map((el, index) => {
+          <Characters key={indexname={el.name} birthyear={el.birthday={el.birth_year}} />
+        } 
+      )} */}
+      {/* props i need to pass and show?? name birth year*/}   
     </div>
   );
 }
